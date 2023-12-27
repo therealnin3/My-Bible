ecb734960b2081e2695488c9a9e722973ae5a84f
GJ Viviers <hardusviviers@gmail.com>
2023-12-27 01:57:16 +0100

    basic layout and IndexedDB setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { openDB } from "idb";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Variables
+  const [book, setBook] = useState(1);
+  const [chapter, setChapter] = useState(1);
+  const [verse, setVerse] = useState(1);
+  const [displayText, setDisplayText] = useState("");
+
+  // Store Bible in IndexedDB
+  useEffect(() => {
+    fetch("/My-Bible/Bible.json")
+      .then((response) => response.json())
+      .then((data) => {
+        openDB("myDatabase", 1, {
+          upgrade(db) {
+            db.createObjectStore("Bible", { keyPath: "id" });
+          },
+        })
+          .then((db) => {
+            const tx = db.transaction("Bible", "readwrite");
+            const store = tx.objectStore("Bible");
+            data.forEach((item, index) => {
+              item.id = index;
+              store.put(item);
+            });
+            return tx.done;
+          })
+          .then(() => {
+            fetchVerse({ book: book, chapter: chapter, verse: verse });
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching Bible.json:", error);
+      });
+  }, []);
+
+  // Fetch verse from IndexedDB
+  async function fetchVerse({ book, chapter, verse }) {
+    console.log("Start fetching verse...");
+    try {
+      const db = await openDB("myDatabase", 1);
+      const tx = db.transaction("Bible", "readonly");
+      const store = tx.objectStore("Bible");
+      const bookData = await store.get(book - 1); // 0-indexed
+      const chapterData = bookData.chapters[chapter - 1]; // 0-indexed
+      const verseData = chapterData[verse - 1]; // 0-indexed
+      setDisplayText(verseData); // Change display text
+    } catch (error) {
+      console.error("Error", error);
+    } finally {
+      console.log("Successfully fetched verse!");
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-screen h-screen p-4 bg-red-500 flex flex-col">
+      <header className="w-full h-28 bg-green-500"></header>
+      <main className="flex flex-1 bg-yellow-500">
+        <div className="flex flex-1 my-4 bg-purple-500 rounded-lg p-4">
+          {displayText}
+        </div>
+      </main>
+      <footer className="w-full h-52 bg-blue-500"></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
