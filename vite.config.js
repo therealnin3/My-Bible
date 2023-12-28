aaecc6c4c5a1ee75db5897ae49b8ee533660b6c9
GJ Viviers <hardusviviers@gmail.com>
2023-12-29 00:02:45 +0100

    added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   base: "/My-Bible/",
 });
